f5d84e71999f23d3f64e9b2f505ff7e6cb06dc77
pasthortown <pastorchessmaster@gmail.com>
2018-11-29 11:34:17 -0500

    navegacion

--- proyecto2/src/app/app.module.ts
+++ proyecto2/src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { PaginaComponent } from './pagina/pagina.component';
+import { OtraComponent } from './otra/otra.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaginaComponent,
+    OtraComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
